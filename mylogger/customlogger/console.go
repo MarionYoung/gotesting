a18a8765c1143d45ac35d6047c5959bbb6e73718
yangc-k <yangc-k@glodon.com>
2020-07-02 17:12:08 +0800

    实现日志输入到终端

--- mylogger/customlogger/console.go
+++ mylogger/customlogger/console.go
@@ -1,9 +1,40 @@
 package customlogger
 
+import (
+	"fmt"
+	"time"
+)
+
 // 将日志内容输出到终端
 
 // Logger 日志结构体
 
 type ConsoleLogger struct {
 	Level LogLevel
+}
+
+// 构造函数
+func NewConsoleLogger(levelStr string) ConsoleLogger {
+	level, err := parseLogLevel(levelStr)
+	if err != nil {
+		panic(err)
+	}
+	return ConsoleLogger{Level: level}
+}
+
+func (c ConsoleLogger) enable(logLevel LogLevel) bool {
+	return logLevel >= c.Level
+}
+
+func (c ConsoleLogger) log(lv LogLevel, format string, a ...interface{}) {
+	if c.enable(lv) {
+		msg := fmt.Sprintf(format, a...)
+		now := time.Now()
+		funcName, fileName, lineNo := getInfo(3)
+		fmt.Printf("[%s] [%s] [%s:%s:%d] %s\n", now.Format("2006-01-02 15:04:05"), getLogString(lv), fileName, funcName, lineNo, msg)
+	}
+}
+
+func (c ConsoleLogger) Debug(format string,a ...interface{}) {
+	c.log(DEBUG, format, a...)
 }
