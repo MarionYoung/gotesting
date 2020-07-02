1de484f63272812587b3a21730a4ac42a4db172e
yangc-k <yangc-k@glodon.com>
2020-07-02 17:34:52 +0800

    实现接口全部方法

--- mylogger/customlogger/console.go
+++ mylogger/customlogger/console.go
@@ -19,7 +19,9 @@ func NewConsoleLogger(levelStr string) ConsoleLogger {
 	if err != nil {
 		panic(err)
 	}
-	return ConsoleLogger{Level: level}
+	return ConsoleLogger{
+		Level: level,
+	}
 }
 
 func (c ConsoleLogger) enable(logLevel LogLevel) bool {
@@ -35,6 +37,21 @@ func (c ConsoleLogger) log(lv LogLevel, format string, a ...interface{}) {
 	}
 }
 
-func (c ConsoleLogger) Debug(format string,a ...interface{}) {
+func (c ConsoleLogger) Debug(format string, a ...interface{}) {
 	c.log(DEBUG, format, a...)
+}
+
+func (c ConsoleLogger) Info(format string, a ...interface{}) {
+	c.log(INFO, format, a...)
+}
+
+func (c ConsoleLogger) Warning(format string, a ...interface{}) {
+	c.log(WARNING, format, a...)
+}
+
+func (c ConsoleLogger) Error(format string, a ...interface{}) {
+	c.log(ERROR, format, a...)
+}
+func (c ConsoleLogger) Fatal(format string, a ...interface{}) {
+	c.log(FATAL, format, a...)
 }
